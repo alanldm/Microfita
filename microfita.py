9a3768089f98ce94a8ff4f1ec090e458eee46af2
Alan <alanlimamedeiross@gmail.com>
2022-06-21 12:58:48 -0300

    Criando a função para se calcular a impedância característica

--- microfita.py
+++ microfita.py
@@ -8,3 +8,9 @@ def e_efetiva(e_relativa, w, h):
 
 def velocidade(e):
     return 300000000/np.sqrt(e)
+
+def impedancia(e, w, h):
+    if w/h <=1:
+        return (60/np.sqrt(e))*np.log(((8*h)/w)+(w/(4*h)))
+    elif w/h >1:
+        return np.sqrt(e)*((w/h)+1.393+0.667*np.log((w/h)+1.444))
