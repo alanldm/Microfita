2040934767ca3c30c69d63f45cff41f999c091b1
Alan <alanlimamedeiross@gmail.com>
2022-06-21 13:04:15 -0300

    Criando a função A

--- microfita.py
+++ microfita.py
@@ -14,3 +14,6 @@ def impedancia(e, w, h):
         return (60/np.sqrt(e))*np.log(((8*h)/w)+(w/(4*h)))
     elif w/h >1:
         return np.sqrt(e)*((w/h)+1.393+0.667*np.log((w/h)+1.444))
+
+def A(z, e_relativa):
+    return (z/60)*np.sqrt((e_relativa+1)/2)+((e_relativa-1)/(e_relativa+1))*(0.23+0.11/e_relativa)
