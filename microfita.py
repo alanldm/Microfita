e29d98b469aa10f306f38e363fa62b0666cda6b1
Alan <alanlimamedeiross@gmail.com>
2022-06-21 12:19:57 -0300

    Criando a função para o cálculo da velocidade de propagação

--- microfita.py
+++ microfita.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 def e_efetiva(e_relativa, w, h):
     if w/h<1:
         return ((e_relativa+1)/2)+((e_relativa-1)/2)*(((1+12*h/w)**(-1/2))+0.04*((1-w/h)**2))
     elif w/h>1:
         return ((e_relativa+1)/2)+((e_relativa-1)/2)*(((1+12*h/w)**(-1/2)))
+
+def velocidade(e):
+    return 300000000/np.sqrt(e)
